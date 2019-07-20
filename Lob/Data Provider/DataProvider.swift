19a23a5d3bf75834e4b083148c2f76036fe25156
ellbosch <elliot.boschwitz@gmail.com>
2019-07-19 18:14:26 -0700

    begun to remove alamofire

--- Lob/Data Provider/DataProvider.swift
+++ Lob/Data Provider/DataProvider.swift
@@ -19,6 +19,13 @@ class DataProvider {
     
     public var sportsData: [String:Sport] = [:]
     
+    // custom error types
+    enum DataProviderError: Error {
+        case apiError
+        case dataResponseError
+        case jsonParseError
+    }
+    
     private init() {
         self.getSportsData()
     }
@@ -80,22 +87,36 @@ class DataProvider {
         var videoPostsUnsorted = [Date: [VideoPost]]()
         var videoPostsSorted = [(Date, [VideoPost])]()
         
-        Alamofire.request(LOB_ROOT_URL + "/new/\(sport)").responseJSON { response in
-            if let data:Data = response.data, let json: JSON = try? JSON(data: data) {
-                let decoder = JSONDecoder()
-                
-                for tuple in json {
-                    // iterate to next post if no valid data found
-                    guard let post_data = try? decoder.decode([VideoPost].self, from: tuple.1.rawData()) else {
-                        continue
-                    }
-                    
-                    for videoPost in post_data {
-                        if videoPostsUnsorted[videoPost.dateShort] != nil {
-                            videoPostsUnsorted[videoPost.dateShort]!.append(videoPost)
-                        } else {
-                            videoPostsUnsorted[videoPost.dateShort] = [videoPost]
-                        }
+        
+        guard let urlRequest = URL(string: LOB_ROOT_URL + "/new/\(sport)") else {
+            print("ERROR: cannot create URL")
+            return
+        }
+
+        let task = URLSession(configuration: .default).dataTask(with: urlRequest) { (data, response, error) in
+            // check for any errors
+            guard error == nil else {
+                print("error calling GET on /todos/1")
+                print(error!)
+                return
+            }
+            // make sure we got data
+            guard let responseData = data else {
+                print("Error: did not receive data")
+                return
+            }
+            
+            // parse valid responseData
+            let decoder = JSONDecoder()
+            do {
+                guard let postData: [VideoPost] = try decoder.decode([String:[VideoPost]].self, from: responseData)["results"] else {
+                    throw DataProviderError.jsonParseError
+                }
+                for videoPost in postData {
+                    if videoPostsUnsorted[videoPost.dateShort] != nil {
+                        videoPostsUnsorted[videoPost.dateShort]!.append(videoPost)
+                    } else {
+                        videoPostsUnsorted[videoPost.dateShort] = [videoPost]
                     }
                 }
                 // sort videos
@@ -104,16 +125,98 @@ class DataProvider {
                     // overwrite old array
                     videoPostsUnsorted[videos.key] = videosSorted
                 }
-                
+
                 // sort dates
                 let sortedKeys = Array(videoPostsUnsorted.keys).sorted(by: >)
                 for key in sortedKeys {
                     let tuple = (key, videoPostsUnsorted[key]!)
                     videoPostsSorted.append(tuple)
                 }
+                completion(videoPostsSorted)
+            } catch {
+                print(error)
             }
-            completion(videoPostsSorted)
         }
+        task.resume()
+            
+            
+//            if let data: Data = data, let json: JSON = try? JSON(data: data) {
+//            let decoder = JSONDecoder()
+//
+//            for tuple in json {
+//                do {
+//                    // iterate to next post if no valid data found
+//                    guard let post_data = try decoder.decode([VideoPost].self, from: tuple.1.rawData()) else {
+//                        continue
+//                    }
+//
+//                    for videoPost in post_data {
+//                        if videoPostsUnsorted[videoPost.dateShort] != nil {
+//                            videoPostsUnsorted[videoPost.dateShort]!.append(videoPost)
+//                        } else {
+//                            videoPostsUnsorted[videoPost.dateShort] = [videoPost]
+//                        }
+//                    }
+//                    // sort videos
+//                    for videos in videoPostsUnsorted {
+//                        let videosSorted = videos.value.sorted(by: { $0.datePosted > $1.datePosted })
+//                        // overwrite old array
+//                        videoPostsUnsorted[videos.key] = videosSorted
+//                    }
+//
+//                    // sort dates
+//                    let sortedKeys = Array(videoPostsUnsorted.keys).sorted(by: >)
+//                    for key in sortedKeys {
+//                        let tuple = (key, videoPostsUnsorted[key]!)
+//                        videoPostsSorted.append(tuple)
+//                    }
+//                    //            }
+//                    completion(videoPostsSorted)
+//                } catch  {
+//                    print("error trying to convert data to JSON")
+//                    return
+//                }
+//            }
+//        }
+//        task.resume()
+            
+            
+            
+            
+//        Alamofire.request(LOB_ROOT_URL + "/new/\(sport)").responseJSON { response in
+//            if let data:Data = response.data, let json: JSON = try? JSON(data: data) {
+//                let decoder = JSONDecoder()
+//
+//                for tuple in json {
+//                    // iterate to next post if no valid data found
+//                    guard let post_data = try? decoder.decode([VideoPost].self, from: tuple.1.rawData()) else {
+//                        continue
+//                    }
+//
+//                    for videoPost in post_data {
+//                        if videoPostsUnsorted[videoPost.dateShort] != nil {
+//                            videoPostsUnsorted[videoPost.dateShort]!.append(videoPost)
+//                        } else {
+//                            videoPostsUnsorted[videoPost.dateShort] = [videoPost]
+//                        }
+//                    }
+//                }
+//                // sort videos
+//                for videos in videoPostsUnsorted {
+//                    let videosSorted = videos.value.sorted(by: { $0.datePosted > $1.datePosted })
+//                    // overwrite old array
+//                    videoPostsUnsorted[videos.key] = videosSorted
+//                }
+//
+//                // sort dates
+//                let sortedKeys = Array(videoPostsUnsorted.keys).sorted(by: >)
+//                for key in sortedKeys {
+//                    let tuple = (key, videoPostsUnsorted[key]!)
+//                    videoPostsSorted.append(tuple)
+//                }
+//            }
+//            completion(videoPostsSorted)
+//        }
     }
     
     // gets sport data from property list
