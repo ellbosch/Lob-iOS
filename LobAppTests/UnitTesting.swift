ecc197da8f8b6216e8d4971cfae92c61bc7e6025
Elliot Boschwitz <elliot.boschwitz@gmail.com>
2019-07-24 20:48:52 -0700

    updated build settings to support testing

--- LobAppTests/UnitTesting.swift
+++ LobAppTests/UnitTesting.swift
@@ -20,9 +20,12 @@ class UnitTesting: XCTestCase {
     }
     
     // MARK: - Tests VideoPost class
-//    func testVideoPost() {
-//        let videoPost = VideoPost(
-//    }
+    func testVideoPostClass() {
+        let videoPost = VideoPost(id: "0", title: "Test", subreddit: "nba", datePostedRaw: "Wed, 24 Jul 2019 23:48:41 GMT", author: "user", redditScore: 5, redditCommentsUrlRaw: "https://reddit.com/r/baseball/comments/ch2qs2", urlRaw: "https://streamable.com/y3on6", mp4UrlRaw: "https://cdn-b-east.streamable.com/video/mp4/0shx7.mp4?token=GDCxhvl0HfI8ZGVF-aBnWQ&expires=1564025520", thumbnailUrlRaw: "https://cdn-b-east.streamable.com/image/0shx7_first.jpg?token=m_BGt9EyRuIE-jvwctqwzg&expires=1564025520", height: 5, width: 9, hotScore: 100.5)
+        
+        // test all valid cases
+        XCTAssertNotNil(videoPost.getDatePostedLong())
+    }
 
     // MARK: - DataProvider helper call
 //    func testBuildUrl() {
